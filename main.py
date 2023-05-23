10b71a949ed9ff615197a381765fee76a867b494
Sheel Shah <sheelshah@Sheels-MacBook-Pro.local>
2023-05-22 20:52:44 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# x = 110
+
+# y = True
+# z = False
+
+# if y == False:
+#     print("x is big")
+#     print("Look at this code here")
+# elif x == 10:
+#     print("x is 10")
+# else:
+#     print ("x is tiny")
+
+# if thing is true ;
+# then do some stuff
+
+# More list stuff
+wh_list = ["apple", "banana", "orange", "spinach", "kale"]
+
+# # [start:end:step]
+# print(wh_list[::2])
+
+# print( f'My favorite foods are {wh_list[0]}s, a sliced {wh_list[2]}, and {wh_list[4]} chips.')
+
+age = 30
+
+print(f'My age is {age} and today is my birthday!')
